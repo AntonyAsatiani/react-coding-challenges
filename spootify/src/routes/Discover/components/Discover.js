de8340115683a686219869b890ddf12a7de145c2
AntonyAsatiani <anton.asatiani@gmail.com>
2024-01-07 19:38:33 +0400

    getting releases, categories and playlists

--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
 import '../styles/_discover.scss';
+
+const spotifyUrl = process.env.REACT_APP_SPOTIFY_URL
+const authString = `${process.env.REACT_APP_SPOTIFY_CLIENT_ID}:${process.env.REACT_APP_SPOTIFY_CLIENT_SECRET}`
+
 
 export default class Discover extends Component {
   constructor() {
@@ -12,10 +17,43 @@ export default class Discover extends Component {
       categories: []
     };
   }
+    async setPlaylist() {
+      const data = await axios.get(`${spotifyUrl}/browse/featured-playlists`, {
+        headers: {
+          Authorization: ``,
+        }
+      });
+      this.setState({playlists: data.data.playlists.items})
+    }
+
+    async setBrowseCategory() {
+      const data = await axios.get(`${spotifyUrl}/browse/categories`, {
+        headers: {
+          Authorization: ``,
+        }
+      });
+      console.log('data :>> ', data);
+      this.setState({categories: data.data.categories.items});
+    }
+
+    async setNewReleases() {
+      const data = await axios.get(`${spotifyUrl}/browse/new-releases`, {
+        headers: {
+          Authorization: ``,
+        }
+      });
+      console.log('data :>> ', data);
+      this.setState({newReleases: data.data.albums.items});
+    }
+
+  componentDidMount() {
+    this.setPlaylist();
+    this.setBrowseCategory();
+    this.setNewReleases();
+  }
 
   render() {
     const { newReleases, playlists, categories } = this.state;
-
     return (
       <div className="discover">
         <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
