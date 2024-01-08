b6e802de9ab6ea4e2e1dd69408047848b255549c
AntonyAsatiani <anton.asatiani@gmail.com>
2024-01-08 21:08:06 +0400

    get auth token

--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -4,8 +4,33 @@ import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
 import '../styles/_discover.scss';
 
 const spotifyUrl = process.env.REACT_APP_SPOTIFY_URL
-const authString = `${process.env.REACT_APP_SPOTIFY_CLIENT_ID}:${process.env.REACT_APP_SPOTIFY_CLIENT_SECRET}`
 
+const sendRequest = async (path) => {
+  const data = await axios.get(`${spotifyUrl}/browse/${path}`, {
+    headers: {
+      Authorization: `Bearer BQA526mUC_8EmQ2ME-Dj35NHtROG1m8JryXQc8xdsDydzYiJ2BTTKb8vy70WhC0LrhsDASQeKBLiX0EfeQeQIjS5Ew3Ow4tU9So_SKuSX_C_L1nRuvs`,
+    }
+  });
+
+}
+
+const getAuthToken = async () => {
+  const data = new URLSearchParams();
+  data.append('grant_type', 'client_credentials');
+  data.append('client_id', process.env.REACT_APP_SPOTIFY_CLIENT_ID);
+  data.append('client_secret', process.env.REACT_APP_SPOTIFY_CLIENT_SECRET);
+
+  const token = await axios.post('https://accounts.spotify.com/api/token', data, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+  });
+  console.log('token :>> ', token.data.access_token);
+  return token.data.access_token;
+}
+( async () => {
+  // await getAuthToken();
+})();
 
 export default class Discover extends Component {
   constructor() {
@@ -20,7 +45,7 @@ export default class Discover extends Component {
     async setPlaylist() {
       const data = await axios.get(`${spotifyUrl}/browse/featured-playlists`, {
         headers: {
-          Authorization: ``,
+          Authorization: `Bearer BQA526mUC_8EmQ2ME-Dj35NHtROG1m8JryXQc8xdsDydzYiJ2BTTKb8vy70WhC0LrhsDASQeKBLiX0EfeQeQIjS5Ew3Ow4tU9So_SKuSX_C_L1nRuvs`,
         }
       });
       this.setState({playlists: data.data.playlists.items})
@@ -29,20 +54,18 @@ export default class Discover extends Component {
     async setBrowseCategory() {
       const data = await axios.get(`${spotifyUrl}/browse/categories`, {
         headers: {
-          Authorization: ``,
+          Authorization: `Bearer BQA526mUC_8EmQ2ME-Dj35NHtROG1m8JryXQc8xdsDydzYiJ2BTTKb8vy70WhC0LrhsDASQeKBLiX0EfeQeQIjS5Ew3Ow4tU9So_SKuSX_C_L1nRuvs`,
         }
       });
-      console.log('data :>> ', data);
       this.setState({categories: data.data.categories.items});
     }
 
     async setNewReleases() {
       const data = await axios.get(`${spotifyUrl}/browse/new-releases`, {
         headers: {
-          Authorization: ``,
+          Authorization: `Bearer BQA526mUC_8EmQ2ME-Dj35NHtROG1m8JryXQc8xdsDydzYiJ2BTTKb8vy70WhC0LrhsDASQeKBLiX0EfeQeQIjS5Ew3Ow4tU9So_SKuSX_C_L1nRuvs`,
         }
       });
-      console.log('data :>> ', data);
       this.setState({newReleases: data.data.albums.items});
     }
 
